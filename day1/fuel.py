54a831d97777de1773825342d0bbd25a4b7ee935
ateexd <ateendraramesh@gmail.com>
2019-12-01 14:40:56 -0500

    minor edits

--- day1/fuel.py
+++ day1/fuel.py
@@ -1,11 +1,10 @@
-def compute(val):
+def compute_1(val):
     return int(val / 3) - 2
 
 def compute_2(val):
     temp = int(val / 3) - 2
     if temp < 0:
         return 0
-
     return  temp + compute_2(temp)
 
 
@@ -14,12 +13,13 @@ if __name__ == "__main__":
     vals = f.read().split('\n')
     ans_1 = 0
     ans_2 = 0
+
     try:
         for val in vals:
-            ans_1 += compute(int(val))
+            ans_1 += compute_1(int(val))
             ans_2 += compute_2(int(val))
     except:
         pass
-    print(compute_2(100756), compute_2(14), compute_2(1969))
+
     print(ans_1, ans_2)
 
