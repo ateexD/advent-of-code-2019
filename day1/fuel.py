5e19c0b5ab6ba3b188484c27a02d448726a77f88
ateexd <ateendraramesh@gmail.com>
2019-12-02 16:57:18 -0500

    minor changes

--- day1/fuel.py
+++ day1/fuel.py
@@ -1,25 +1,21 @@
 def compute_1(val):
     return int(val / 3) - 2
 
+
 def compute_2(val):
     temp = int(val / 3) - 2
     if temp < 0:
         return 0
-    return  temp + compute_2(temp)
+    return temp + compute_2(temp)
 
 
 if __name__ == "__main__":
     f = open('input.txt', 'r')
-    vals = f.read().split('\n')
-    ans_1 = 0
-    ans_2 = 0
+    vals = f.read().strip().split('\n')
 
-    try:
-        for val in vals:
-            ans_1 += compute_1(int(val))
-            ans_2 += compute_2(int(val))
-    except:
-        pass
+    ans_1 = sum((compute_1(int(val)) for val in vals))
+    ans_2 = sum((compute_2(int(val)) for val in vals))
 
     print(ans_1, ans_2)
     f.close()
+
